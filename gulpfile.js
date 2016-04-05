267f8bc448be4be91e32644d8bbe9b9238748da6
Graham F. Scott <gfscott@gmail.com>
2016-04-05 16:49:48 -0400

    adding gulp-inline-src to reduce render blocking

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require("gulp"),
     // developer has deprecated and recommended cssnano instead
     // cssmin = require("gulp-minify-css"),
     cssnano = require("gulp-cssnano"),
+    inlinesrc = require("gulp-inline-source"),
     htmin = require("gulp-htmlmin"),
     uglify = require("gulp-uglify");
 
-gulp.task("default", ["scripts", "styles", "html", "images"]);
+gulp.task("default", ["scripts", "styles", "html", "images", "inline"]);
 
 gulp.task("watch", function(){
   gulp.watch("./src/js/*.js", ["scripts"]);
@@ -17,26 +18,26 @@ gulp.task("watch", function(){
 });
 
 gulp.task("scripts", function(){
-  
+
   gulp.src("./src/js/*.js")
       .pipe(concat("script.js"))
       .pipe(uglify())
       .pipe(gulp.dest("./dist/js"));
-  
+
 });
 
 gulp.task("styles", function(){
-  
+
   gulp.src("./src/scss/*.scss")
       .pipe(concat("style.css"))
       .pipe(sass())
       .pipe(cssnano())
       .pipe(gulp.dest("./dist/css"));
-  
+
 });
 
 gulp.task("html", function(){
-  
+
   gulp.src("./src/**/*.html")
       .pipe(htmin({
         collapseWhitespace: true,
@@ -44,13 +45,22 @@ gulp.task("html", function(){
         minifyJS:true
         }))
       .pipe(gulp.dest("./dist"));
-  
+
 });
 
 
 gulp.task("images", function(){
-  
+
   gulp.src("./src/img/*")
       .pipe(gulp.dest("./dist/img"));
-  
+
+});
+
+// Gulp task to inline css for faster rendering
+gulp.task("inline", function(){
+
+gulp.src("./dist/**/*.html")
+  .pipe(inlinesrc())
+  .pipe(gulp.dest("./dist"));
+
 });
